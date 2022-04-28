f2f58e7759f7ba7c435e08e521a6a51e0768c9ea
Andrew Crane <andrew.crane2802@gmail.com>
2022-04-27 20:09:34 -0500

    filament setup 1

--- Titan/Renderer/src/Renderer.cpp
+++ Titan/Renderer/src/Renderer.cpp
@@ -1,10 +1,25 @@
 #include "Renderer.h"
+#include "utils/EntityManager.h"
+#include "filament/Engine.h"
+#include "filament/SwapChain.h"
+#include "filament/Renderer.h"
+#include "filament/View.h"
 
 namespace Titan
 {
 
 	bool Renderer::init(GLFWwindow* win)
 	{
+		filament::Engine* engine = filament::Engine::create();
+		filament::SwapChain* swapChain = engine->createSwapChain(glfwGetWin32Window(win));
+		filament::Renderer* renderer = engine->createRenderer();
+
+		filament::Camera* camera = engine->createCamera(utils::EntityManager::get().create());
+		filament::View* view = engine->createView();
+		filament::Scene* scene = engine->createScene();
+
+		view->setCamera(camera);
+		view->setScene(scene);
 		return true;
 	}
 
